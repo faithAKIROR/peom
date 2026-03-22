af580b18cedb637c1179f079abf74a90025af031
Faith-tech-code <akirorfaith45@gmail.com>
2026-03-22 16:24:18 +0300

    ai added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
+function displayPoem(response) {
+    console.log("poem generated");
+    new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+});
+}
 function generatePoem(event) {
     event.preventDefault();
+        
+        let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "90796ab239dfftd1f27ee5coe1d1e41b";
+    let context = "You are a romantic Poem expert and love to write short poems. Your mission is to write four line poem in basic HTML and seperate each line with a <br/> dont include the word html at the begining. Make sure to follow the user instructions bellow.Sign the poem with 'SheCodes AI' inside a <strong> tag at the end of the poem.";
+    let prompt = `User instructions: Generate a poem about ${instructionsInput.value} in French`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    console.log("Generating poem...");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context: ${context}`);
+    
+
+    axios.get(apiURL).then(displayPoem);
 
     new Typewriter("#poem", {
     strings:" La tombe dit à la rose",
